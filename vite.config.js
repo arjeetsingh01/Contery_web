d275f0bf1ba4ad62ca7e443a601b3c568fb9e017
Arjeet Singh <deshpratapyadav@gmail.com>
2025-07-06 09:06:31 +0530

    fix: updated homepage and vite base config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/countrys/', // IMPORTANT: this must match your repo name
+  base: '/conter_site/', // 👈👈👈 this must match repo name exactly
   plugins: [react()],
-})
+});
